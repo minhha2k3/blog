563f39cd566255ed58a645000d3d716c522729d9
minhha2k3 <hadep7a@gmail.com>
2023-05-10 22:17:46 +0700

    Read from DB

--- src/app/controllers/SiteController.js
+++ src/app/controllers/SiteController.js
@@ -1,19 +1,17 @@
 const Course = require('../models/Course');
+const { multipleMongooseToObject } = require('../../utils/mongoose')
 
 class SiteController {
     //[GET] /
-    index(req, res) {
-
-        Course.find({}, function(err, courses) {
-            if(!err) {
-                res.json(courses);
-            } else {
-                res.status(400).json({ err: 'ERROR!!!' });
-            }
-
-        });
-
-        //   res.render('home');
+    index(req, res, next) {
+        Course.find({}).lean()
+            .then(courses => {
+                res.render('home', { courses });
+                // res.render('home', { 
+                //     courses: multipleMongooseToObject(courses);
+                // });
+            })
+            .catch(next);
 
     }
 
--- src/app/models/Course.js
+++ src/app/models/Course.js
@@ -1,6 +1,7 @@
 const mongoose = require('mongoose');
 
 const Schema = mongoose.Schema;
+mongoose.set('strictQuery', false);
 
 const Course = new Schema({
   name: {type: String, maxLength: 255},
